6b968c6bcf7977dccadb3c0ff715f9f4e46c9eca
Shadabkshaikh86 <shadabkashaikh439@gmail.com>
2024-10-23 00:33:19 +0530

    first commit

---
+++ partner_preferance_details/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PartnerPreferanceDetailsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'partner_preferance_details'
